e2adbc8cbe39e5641afcf56ddc7e0f206f9b716d
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-03-08 12:57:58 -0800

    TodoList component is connected to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import TodoList from './components/TodoList'
+
+const todoItems= [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false,
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false,
+  },
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      todoItems: todoItems,
+    }
+  }
+    
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
+import React from  'react'
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+class TodoList extends React.Component {
+
+    render(){
+        return(
+            <>
+            <h3>TodoList is connected</h3>
+            </>
+        )
+    }
+}
+export default TodoList;
