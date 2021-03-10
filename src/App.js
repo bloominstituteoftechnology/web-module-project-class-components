3525867e50259008403b8b34f056b99dccc00983
Tony Sorensen <tony48853@gmail.com>
2021-03-10 14:22:16 -0800

    initial commit. buttons and input field added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  //   this.state={
+  //     todos: todos
+  //   }
+  // }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <input type="text" placeholder="Add todo item"></input>
+        <button>Add Todo</button>
+        <button>Clear Completed</button>
       </div>
     );
   }
