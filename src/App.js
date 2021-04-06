201d2dec47bdaa165085994205ebbda72f92012f
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-04-05 21:06:46 -0400

    Linked up CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import './todo.css'
 
 const todos = [
   {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import './todo.css'
 ReactDOM.render(<App />, document.getElementById('root'));
