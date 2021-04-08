43338e3db4d6c43f6ae3669eff91fc8f16251079
miketillman95 <matillman95@gmail.com>
2021-04-08 16:21:17 -0700

    update

---
+++ src/components/TodoForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todo from './Todo'
+
+const TodoForm = props =>{
+    <div className='todo-list'>
+        {props.task.map(todo => (
+            <Todo toggleCompleted={props.toggleCompleted} key={todo.id} todo={todo}/>
+        ))}
+
+        <button onClick={()=> props.clearCompleted()} className= 'clr-btn'>
+            
+        </button>
+    </div>
+    
+}
