d0de378d3d531cf39e3cd5d56f18b25159c1f397
artofmayhem <chef_tony_miller@yahoo.com>
2021-04-07 15:19:39 -1000

    MVP complete

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,21 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import useStyles from "../stylesheets/useStyles";
+import Todo from "./Todo";
+
+const TodoList = (props) => {
+  const classes = useStyles();
+
+  console.log("Received props from TodoList", props);
+  console.log(classes);
+
+  return (
+    <div className='d-flex flex-column justify-content-start'>
+      <div className="d-flex justify-content-start" style={{justifySelf: 'start'}}>
+        <Todo props={props} />
+      </div>
+      );
+    </div>
+  );
+};
+
+export default TodoList;
