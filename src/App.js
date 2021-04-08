22ab8509b3458e55250cebfe2c30b702a8df830f
Nathan Chagar <nathan.adel.chagar@gmail.com>
2021-04-07 22:05:35 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Todo from './components/TodoForm'
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List: MVP</h1>
+        <Todo/>
+
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todo = props => {
+    return(
+        <input 
+         type='text'
+        />
+    ) 
+ }
+
+ export default Todo
