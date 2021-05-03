c09253ebf48c0f8288ce5ea166e5e67528bf1851
Edison Jeon <edison-jeon@lambdastudents.com>
2021-05-03 14:51:07 -0700

    studying now!

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -13,13 +13,17 @@ class TodoForm extends React.Component {
     handleChanges = event => {
         this.setState({
             input: event.target.value
-            
         })
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addItem(this.state.input)
+    }
+    
     render() {
         return (
-            <form>
+            <form onSubmit = {this.handleSubmit}>
                 <input onChange = {this.handleChanges} type = 'text' name = 'item' />
                 <button className = 'add-button'>ADD</button>
             </form>
