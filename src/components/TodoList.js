a56a71544dda71a486e03748c2679c56cc5b87c5
Zachary Cooremans <zcooremans@gmail.com>
2021-05-03 15:06:43 -0500

    MVP

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,16 @@
+import React from 'react'
+import Todo from './Todo'
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+const TodoList = props => {
+    return(
+        <div className='Todo-list'>
+            {props.tasks.map(item => (
+                <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
+            ))}
+        </div>
+    )
+}
+
+export default TodoList
