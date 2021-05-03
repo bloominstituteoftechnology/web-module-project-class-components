b74f2c9051456049304d138466e33c9847b5003e
MurrayWarnock <murray.warnock@gmail.com>
2021-05-03 08:11:33 -0600

    Trying to create github PR

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
-    this.todoList = {
+    this.todoListState = {
       task: "",
       id: 0,
       completed: false
