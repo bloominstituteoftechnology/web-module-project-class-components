cb00a2876d21c22341d92fbb6c6fd6cf9585202f
adampatt <adampattison08@gmail.com>
2021-05-31 22:13:20 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      task: "",
+    }
+  }
   render() {
+    handleOnClick = () => {
+      this.setState()
+    }
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1> ToDo List: MVP</h1>
+        Input <input type="text"></input>
+        <button>Submit</button>
+        <button>Remove</button>
       </div>
     );
   }
 }
 
 export default App;
+
+// On the submit I want to create a new entry and it to be posted above the current Text.
