eea8aa4d4274913e7824b8a8f4dfbe2171049c1e
Lirong <lirongcoding@gmail.com>
2021-06-05 02:17:35 -0700

    mvp

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+
+
+const TodoList = props => {
+
+
+    return (
+        <div className="todolist">
+            {props.todolist.map(todo => (
+                <Todo key={todo.id} todo={todo} toggleCompleted={props.toggleCompleted} />
+            ))}
+            <button className="clear-btns" onClick={props.todolist.filter(todo => todo.completed === false)}>
+                Clear Completed
+            </button>
+        </div>
+    );
+};
+
+export default TodoList;
