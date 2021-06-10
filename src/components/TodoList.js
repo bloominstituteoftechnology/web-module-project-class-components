3f7624d6f9622c80bb4e8744618ec04fadf7aa7c
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-06-10 15:29:21 -0400

    trying to fix to deploy

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Todo from "./Todo"
 
 const TodoList = props =>{
-
+// eslint-disable-next-line
     const handleClick =()=>{
         props.clearCompleted();
     }
