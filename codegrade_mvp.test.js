c070df47d4bc1ec7e34e232b5cfd79e6d3064b9d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-21 15:40:19 +0200

    tests

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -16,17 +16,17 @@ afterEach(() => {
   document.body.innerHTML = ''
 })
 
-test('heading is present', async () => {
+test('[1] heading is present', async () => {
   expect(screen.queryByText('React Codegrade Assignment')).toBeInTheDocument()
 })
 
-test('todos are present', async () => {
+test('[2] todos are present', async () => {
   expect(await screen.findByText(/laundry/)).toBeInTheDocument()
   expect(await screen.findByText(/dishes/)).toBeInTheDocument()
   expect(await screen.findByText(/groceries/)).toBeInTheDocument()
 })
 
-test('can do laundry', async () => {
+test('[3] can do laundry', async () => {
   expect(await screen.findByText('laundry pending')).toBeInTheDocument()
   fireEvent.click(screen.getAllByText('complete')[0])
   expect(await screen.findByText('laundry DONE')).toBeInTheDocument()
