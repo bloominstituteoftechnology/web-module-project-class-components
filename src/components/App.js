0c78871cdcb818c768b81680b4be9f2799d49d98
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-07-29 16:23:47 -0400

    adds in some base router code.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import styled from 'styled-components'
 import axios from 'axios'
 
@@ -8,34 +9,27 @@ const StyledApp = styled.div`
   button { margin-left: 4px; }
 `
 
+
+const LambdaHeader = ()=> {
+  return(<div>Lambda Header</div>);
+}
+
+const Header = ()=> {
+  return(<div>App Header</div>);
+}
+
+const LoginForm = ()=> {
+  return(<div id="Login">Login Form</div>);
+}
+
 export default function App() {
-  const [todos, setTodos] = useState([])
-
-  const getAll = () => fetch('http://localhost:5000/api/todos')
-    .then(res => res.json())
-    .then(data => setTodos(data))
-
-  const toggle = id => axios.patch(`http://localhost:5000/api/todos/${id}`)
-    .then(res => setTodos(res.data))
-
-  useEffect(() => { getAll() }, [])
-
   return (
-    <StyledApp>
-      <h1>Todos</h1>
-      <h3>React Codegrade Assignment</h3>
-      <ul>
-        {
-          todos.map(todo => (
-            <li key={todo.id}>
-              <span>{todo.name} {todo.complete ? 'DONE' : 'pending'}</span>
-              <button onClick={() => toggle(todo.id)}>
-                {todo.complete ? 'not' : 'complete'}
-              </button>
-            </li>
-          ))
-        }
-      </ul>
-    </StyledApp>
+    <div>
+      <LambdaHeader/>
+      <div id="app">
+        <Header/>
+        <LoginForm/>
+      </div>
+    </div>
   )
 }
