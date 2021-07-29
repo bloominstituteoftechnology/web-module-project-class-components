6d3a257bc89d284cd5549b2df796b296d15b3daa
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-07-29 12:46:51 -0400

    connected endpoints.

--- src/mocks/api.js
+++ src/mocks/api.js
@@ -8,22 +8,27 @@ api.use(express.json())
 
 api.use(cors())
 
+//Get All Posts Endpoint
 api.get('/api/posts', (req, res) => {
   res.json(Posts.getAll())
 })
 
+//Get Post Endpoint
 api.get('/api/posts/:id', (req, res) => {
   res.json(Posts.getById(req.params.id))
 })
 
+//Create Post Endpoint
 api.post('/api/posts', (req, res) => {
   res.json(Posts.create(req.body))
 })
 
+//Edit Post Endpoint
 api.put('/api/posts/:id', (req, res) => {
-  res.json(Posts.toggleDone(req.params.id))
+  res.json(Posts.edit(req.params.id, req.body))
 })
 
+//Delete Post Endpoint
 api.delete('/api/posts/:id', (req, res) => {
   res.json(Posts.remove(req.params.id))
 })
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,40 +1,40 @@
 import { rest } from 'msw'
-import Todo from './data'
+import Posts from './data'
 
 function getAll(req, res, ctx) {
   return res(
-    ctx.json(Todo.getAll()),
+    ctx.json(Posts.getAll()),
   )
 }
 
 function getById(req, res, ctx) {
   return res(
-    ctx.json(Todo.getById(req.params.id)),
+    ctx.json(Posts.getById(req.params.id)),
   )
 }
 
 function create(req, res, ctx) {
   return res(
-    ctx.json(Todo.create(req.body)),
+    ctx.json(Posts.create(req.body)),
   )
 }
 
-function toggleDone(req, res, ctx) {
+function edit(req, res, ctx) {
   return res(
-    ctx.json(Todo.toggleDone(req.params.id)),
+    ctx.json(Posts.edit(req.params.id, req.body)),
   )
 }
 
 function remove(req, res, ctx) {
   return res(
-    ctx.json(Todo.remove(req.params.id)),
+    ctx.json(Posts.remove(req.params.id)),
   )
 }
 
 export const handlers = [
-  rest.get('http://localhost:5000/api/todos', getAll),
-  rest.get('http://localhost:5000/api/todos/:id', getById),
-  rest.post('http://localhost:5000/api/todos', create),
-  rest.patch('http://localhost:5000/api/todos/:id', toggleDone),
-  rest.delete('http://localhost:5000/api/todos/:id', remove),
+  rest.get('http://localhost:5000/api/posts', getAll),
+  rest.get('http://localhost:5000/api/posts/:id', getById),
+  rest.post('http://localhost:5000/api/posts', create),
+  rest.put('http://localhost:5000/api/posts/:id', edit),
+  rest.delete('http://localhost:5000/api/posts/:id', remove)
 ]
