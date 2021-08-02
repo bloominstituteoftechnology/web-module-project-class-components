1e3391931eb839e528b237ad63cff9ff13611851
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-08-02 17:32:37 -0600

    Finished.

---
+++ src/components/TodoForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+  handleChanges = (e) => {
+    this.setState({
+      input: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTask(this.state.input);
+  };
+
+  render() {
+    return (
+      <form>
+        <input onChange={this.handleChanges} type="text" name="item" />
+        <button onClick={this.handleSubmit}>Add Todo</button>
+        <br />
+        <button onClick={this.props.clearCompleted}>Clear Completed</button>
+      </form>
+    );
+  }
+}
+export default TodoForm;
