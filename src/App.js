943767e12e4e699a24a0fd2fd965ebb9d4bd7d47
Klove Adams <kalebsxott@gmail.com>
2021-08-02 15:51:38 -0400

    add mocktasks & constructor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+const tasks = [
+  {
+    task: "Do Dishes",
+    id: 123,
+    completed: false,
+  },
+  {
+    task: "Sweep",
+    id: 124,
+    completed: false
+  },
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      tasks: tasks
+    }
+  }
+
   render() {
     return (
       <div>
