487895ad2288afe75dc63985cc690c8994a27c48
Bimisola Daniel <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-08-03 07:00:49 +0100

    I'm still working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 
+
+
+const data = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 class App extends React.Component {
+  constructor() {
+    super();
+  const App= {}={ 
+    const [state, setState] = useState({
+
+    })
+    
+    }
+  }
+  
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
       </div>
     );
   }
