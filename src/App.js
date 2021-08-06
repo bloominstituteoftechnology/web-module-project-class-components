c23df6e2d8f72fe06b1a49edc0b8da9f9e8a2235
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-08-06 11:02:49 -0500

    added state and constructor to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = ({
+      task: 'make dinner',
+      id: Date.now(),
+      completed: false
+    })
+}
+
+
   render() {
     return (
       <div>
@@ -13,6 +23,7 @@ class App extends React.Component {
         </label>
         <button type="submit">Add Task</button>
         </form>
+        <div>{this.state.task}</div>
       </div>
 
     );
