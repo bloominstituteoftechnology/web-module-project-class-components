3cfc0efa30159bbd1366aa8307456164193bd3f6
Rick <rmansfield@realpeoplesratings.com>
2021-08-29 14:42:31 -0500

    safety push lots done without pushing yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
-
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+const todos = [
+  {
+    todo: 'task',
+    id: 0,
+    completed: false
+  }
+]
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    }
+  }
+
+  toggleTodo = id => {
+    console.log('id of todo in app.js', id);
+  }
+
+  addTodo = newestTodo =>{
+    console.log('"App.js todo:', newestTodo);
+    const newTodo = {
+      todo: newestTodo,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+      ...this.state, 
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div>
+        <h2>Todo List</h2>
+        <TodoForm addTodo={this.addTodo}/>
+        </div>
+        <TodoList />
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+class TodoForm extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            input:''
+        }
+    }
+    //The state starts as an empty string. 
+    //the handeChanges fuction deals with grabing what the user inputs to the field. it doesn't "submit" that imput it just "collects" it and changes the state to the inputed data from th eempty string to whatever the user types in. 
+    handleChanges = e => {
+        this.setState({
+            input: e.target.value
+        });
+        
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addItem(this.state.input);
+    }
+    render() {
+
+        return(
+            <form>
+                <input onChange={this.handleChanges} type="text" name="todo" placeholder="type task here "/>
+                <button>Add Todo</button>
+            </form>
+        );
+    }
+}
+
+export default TodoForm; 
