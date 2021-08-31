4a346b35506f9849d31903b7a8be3c3cb7609cfe
unknown <logicndepth@gmail.com>
2021-08-30 21:08:32 -0700

    components completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,2 @@
-import React from "react";
 
-class Todo extends React.Component{
-    
-
-    render(){
-        return(
-            <div>
-               {this.props.task.completed 
-               ? (<strike>{this.props.task.task}</strike>)
-                : this.props.task.task
-            }
-            </div>
-        )
-    }
- }
- export default Todo
 
