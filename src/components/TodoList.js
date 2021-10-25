992612d806b2b62a14369cadb8b239887f8c97c3
Peter Conley <peterdavidconley@gmail.com>
2021-10-25 16:18:02 -0400

    created constructor function, state, and todo list data

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,34 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const toDoList = [
+
+    {
+      task: 'Clean Room',
+      id: 1001,
+      completed: false
+    },
+    {
+      task: 'Vacuum',
+      id: 1002,
+      completed: false
+    },
+    {
+        task: 'Meal Prep',
+        id: 1003,
+        completed: false
+      },
+      {
+        task: 'Plan Weekend',
+        id: 1004,
+        completed: false
+      },
+      {
+        task: 'Brush Teeth',
+        id: 1005,
+        completed: false
+      },
+
+  ];
+
+  export default toDoList
