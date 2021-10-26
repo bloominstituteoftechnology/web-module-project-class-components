f80a94b47aa705c0df0b4edce8c8d9a21c7bc8d9
FftyShadesofCode <philiprmcdavid@gmail.com>
2021-10-26 00:30:46 -0400

    first and final push
    Took 1 hour 47 minutes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import './components/Todo.css';
+import TodoList from './components/TodoList';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+function App() {
+  return (
+      <div className='todo-app'>
+        <TodoList />
       </div>
-    );
-  }
+  );
 }
 
 export default App;
