3d5015538552b06824a2db14c19a3faa9b8ff512
Andrew Gary <andrew.gary91@gmail.com>
2022-02-07 15:06:31 -0600

    set up state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,28 @@
 import React from 'react'
+import axios from 'axios';
 
+
+const initialState = {
+  toDos: [],
+  error: ''
+};
 export default class App extends React.Component {
+  
+  state = initialState;
+
+  componentDidMount() {
+    axios.get('http://localhost:9000/api/todo')
+    .then(resp => {
+      console.log('resp: ', resp);
+      this.setState({...this.state, toDos: resp.data.data});
+    })
+    .catch(error => {
+      console.log('error: ',error.message);
+      this.setState({...this.state, error: error.message})
+    })
+  }
+
+
   render() {
     return (
       <div>
