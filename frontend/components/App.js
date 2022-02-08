dc363c7ecdaeff559d063bd9af546c90b35324eb
Visnu <vgcaisse@gmail.com>
2022-02-07 23:17:22 -0500

    built a map and a other

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,15 +1,41 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
   render() {
+    console.log(this.state);
+
+    const { todos } =  this.state; 
+    console.log(todos);
+    
     return (
       <div>
         <h1>Todo App</h1>
 
         <ul>
-          <li>item 1</li>
-          <li>item 2</li>
-          <li>item 3</li>
+          {
+            todos.map(todo => {
+              return (<li >{todo.task}</li>) 
+            })
+          }
         </ul>
 
         <form>
