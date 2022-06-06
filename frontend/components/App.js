91ba31794972eac8ad70894eae746132354ef8d5
RealSadLemon <esmodearburk@gmail.com>
2022-06-06 18:42:03 -0500

    First commit, Created state in constructor and imported TodoList Component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import TodoList from './TodoList';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    const date = new Date();
+    const timestamp = date.getTime();
+    this.state = [
+      {
+        name: 'Brush Teeth',
+        id: timestamp,
+        completed: false
+      }
+    ];
+  }
+
+
+
   render() {
     return (
-      <div>
-        Todo App
+      <div className='App'>
+        <TodoList todoItems={this.state} />
       </div>
     )
   }
