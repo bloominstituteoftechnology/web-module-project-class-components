ec84e8f407a212ee5525119038adf2af36802bad
JordonP123 <jordonaren@gmail.com>
2022-08-08 06:03:06 -0400

    done

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
+    console.log(this.props.theListArr.completed)
     return (
       <div>
-        Form
+        <form onSubmit={this.props.onSubmit}>
+          <label>Add a chore</label> <br />
+          <input
+          value={this.props.itemText}
+            onChange={this.props.onChange}
+          /> <br />
+          <button>Add</button>
+          <button onClick={this.props.filterCompleted}>clear</button>
+        </form>
       </div>
     )
   }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,10 +2,20 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
+    console.log(this.props.list.completed)
     return (
       <div>
-        Todo
+        <a href='#' onClick={() => this.props.toggleCompleted(this.props.list.id)}>{this.props.list.name}{this.props.list.completed === true ? '--Completed' : null}</a>
       </div>
     )
   }
 }
+
+
+// {
+//   this.props.theListArr.map(list => (
+//           <div key={list.id}>
+//             {list.name}
+//             </div>
+//     ))
+//  }
