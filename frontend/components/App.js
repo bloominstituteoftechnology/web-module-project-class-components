40e2423d91a89a282d3ba3b469abf5b5f50047a0
Texi Schaeffer <texirae.schaeffer@gmail.com>
2023-01-30 13:41:29 -0600

    added in all dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo App</h1>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Take out trash</li>
+          <li>Workout</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
