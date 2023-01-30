dacba73c8c70796b1798a34cbc7616d1913993ac
Texi Schaeffer <texirae.schaeffer@gmail.com>
2023-01-30 13:57:34 -0600

    added todo state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,43 @@
 import React from 'react'
 
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Walk the dog',
+          id: 1234567,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 9876543,
+          completed: false
+        },
+        {
+          task: 'Organize Garage',
+          id: 1357913,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+    console.log(todos);
+
     return (
       <div>
         <h1>Todo App</h1>
+
         <ul>
-          <li>Walk the dog</li>
-          <li>Take out trash</li>
-          <li>Workout</li>
+          {
+            todos.map(todo => {
+              return(<li>{todo.task}</li>)
+            })
+          }
         </ul>
 
         <form>
