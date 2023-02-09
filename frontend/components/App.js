e814984eecf48d36ee480f252c166ddc21e7d3eb
Anthony Climer <anthonyclimer0925@gmail.com>
2023-02-08 20:40:45 -0500

    Frame work done :)

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,21 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+
+
+        <ul>
+          <li>Brush teeth</li>
+          <li>Take out the dogs and feed them</li>
+          <li>Use restroom</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
