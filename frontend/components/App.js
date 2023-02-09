9ad1ec6230276b6afa9c0fb66dae0e180b041610
Anthony Climer <anthonyclimer0925@gmail.com>
2023-02-08 21:17:15 -0500

    I have the dummy data rendering in the dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,38 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos:[
+        {
+          name: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
   render() {
+    const {todos} = this.state
+    
     return (
       <div>
         <h1>Todos</h1>
 
 
         <ul>
-          <li>Brush teeth</li>
-          <li>Take out the dogs and feed them</li>
-          <li>Use restroom</li>
+          {
+            todos.map(todo => {
+              return (<li>{todo.name} {todo.completed?<span>completed</span> : <span></span>}</li>)
+            })
+          }
         </ul>
 
         <form>
