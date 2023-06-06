348179ed082af3ab9317e915976a380cff26b007
DJ1 <jimmynoble604@gmail.com>
2023-06-06 14:42:55 -0400

    most of the challenge is satup.

---
+++ component/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./todo.gif/components";
+import "./frontend/components";
+
+
+// have a onclick button for "completed".
+// have a "submit" button.
+// have a "clear" button.
