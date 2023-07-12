9d1262acf4ef0f35e9c726c0d29be7c27b79f1b5
Jomar Banting <shadowlover.05@gmail.com>
2023-07-12 04:13:46 -0400

    build from state and completed flags

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,39 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+      {
+        name: 'Organize Garage',
+        id: 1528817077286, // could look different, you could use a timestamp to generate it
+        completed: false
+      },
+      {
+        name: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false
+      },
+    ]
+    }
+  }
   render() {
+    const { todos } = this.state;
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+        <ul>
+          {todos.map(element => {
+            return (<li key={element.id}>{element.name} {todos.completed?<span>- completed</span> : <span></span>}</li>)
+          })}
+        </ul>
+
+        <form>
+          <input />
+          <buttom>Add</buttom>
+        </form>
+        <button>Clear</button>
       </div>
     )
   }
